dd9e2462522fefb51a5e48b669cc0dd693079541
Максим Польченко <maximus9279258@gmail.com>
2023-04-03 16:44:16 +0300

    first commit  webpack config/ main config / css config modules

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,33 @@
+import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const sccLoaders = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:8]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [typescriptLoader, sccLoaders];
+}
