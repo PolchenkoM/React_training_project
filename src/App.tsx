83056b8377ce8d98b8aa7dda006d3f1d6cf9edde
Максим Польченко <maximus9279258@gmail.com>
2023-04-04 18:39:53 +0300

    create classNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import "./styles/index.scss";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
-import { useTheme } from "./theme/UseTheme";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme()
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {hovered: true, selected: false}, [theme, 'cls2'])}>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <button onClick={toggleTheme}>Toggle</button>
