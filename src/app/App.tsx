4aaa2d918d28219ef75f94e8184b0f2afb526039
Максим Польченко <maximus9279258@gmail.com>
2023-04-07 16:02:42 +0300

    add eslint config / fix all files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,28 @@
-import React, { Suspense } from "react";
-import "./styles/index.scss";
-import { Link } from "react-router-dom";
-import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "app/providers/ThemeProvider";
-import AppRouter from "./providers/router/ui/AppRouter";
-import { Navbar } from "widgets/Navbar";
-import { Sidebar } from "widgets/Sidebar";
-
+import React, { Suspense } from 'react';
+import './styles/index.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import AppRouter from './providers/router/ui/AppRouter';
 
 export const App = () => {
-  const { theme } = useTheme();
+    const { theme } = useTheme();
 
-  return (
-    <div
-      className={classNames("app", { hovered: true, selected: false }, [
-        theme,
-        "cls2",
-      ])}
-    >
-      <Suspense fallback="">
-        <Navbar />
-        <div className="content-page">
-          <Sidebar />
-          <AppRouter />
+    return (
+        <div
+            className={classNames('app', { hovered: true, selected: false }, [
+                theme,
+                'cls2',
+            ])}
+        >
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
-      </Suspense>
-    </div>
-  );
+    );
 };
