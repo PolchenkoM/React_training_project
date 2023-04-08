0961a3177106746a8770049f6ea16f23bfc96924
Максим Польченко <maximus9279258@gmail.com>
2023-04-08 10:34:41 +0300

    add page loader

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import './Loader.scss';
+
+interface LoaderProps {
+    className?: string
+}
+
+const Loader = ({ className }:LoaderProps) => (
+    <div className={classNames('Loader', {}, [className])}>
+        <div />
+        <div />
+    </div>
+);
+
+export default Loader;
